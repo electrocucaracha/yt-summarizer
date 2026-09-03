e94b1876bf40b2ef4814efc9d545939ac5ba79aa
Victor Morales <chipahuac@hotmail.com>
2026-09-02 18:46:44 -0700

    fix(ci): simplify sed expressions for tag extraction in update_versions.sh
    Refactor sed commands to remove GitHub tag prefixes and suffixes more
    cleanly when parsing refs from git ls-remote. Removes overcomplicated
    patterns, reducing risk of mangling tag names and improving readability.
    No functional change intended.

--- ci/update_versions.sh
+++ ci/update_versions.sh
@@ -62,9 +62,9 @@ for action in $gh_actions; do
         continue
     fi
     if [[ ${exceptions[*]} =~ (^|[^[:alpha:]])$action([^[:alpha:]]|$) ]]; then
-        commit_hash=$(git ls-remote "https://github.com/$action" | grep 'refs/tags/[v]\?[0-9][0-9\.]*\^{}$' | sed 's|refs/tags/[vV]\?[\.]\?||g; s|\^{}$||g' | sort -u -k2 -V | tail -1 | awk '{ printf "%s # %s\n",$1,$2 }')
+        commit_hash=$(git ls-remote "https://github.com/$action" | grep 'refs/tags/[v]\?[0-9][0-9\.]*\^{}$' | sed 's|refs/tags/||; s|\^{}$||' | sort -u -k2 -V | tail -1 | awk '{ printf "%s # %s\n",$1,$2 }')
     else
-        commit_hash=$(git ls-remote "https://github.com/$action" | grep 'refs/tags/[v]\?[0-9][0-9\.]*$' | sed 's|refs/tags/[vV]\?[\.]\?||g' | sort -u -k2 -V | tail -1 | awk '{ printf "%s # %s\n",$1,$2 }')
+        commit_hash=$(git ls-remote "https://github.com/$action" | grep 'refs/tags/[v]\?[0-9][0-9\.]*$' | sed 's|refs/tags/||' | sort -u -k2 -V | tail -1 | awk '{ printf "%s # %s\n",$1,$2 }')
     fi
     # shellcheck disable=SC2267
     grep -ElRZ "uses: $action@" .github/ | xargs -0 -l sed -i -e "s|uses: $action@.*|uses: $action@$commit_hash|g"
